3fb033ad97eb9faa67842f3385fbc728d5596f2e
Constantin von Crailsheim <constantin@vcrailsheim.de>
2023-03-02 19:19:24 +0100

    Initialize repo and checked out data

---
+++ experiments.py
@@ -0,0 +1,24 @@
+# %%
+
+from data import Dataset
+import numpy as np
+import pandas as pd
+
+# %%
+
+data = Dataset.from_openml(976)
+
+X = data.features.to_numpy()
+y = data.labels.to_numpy()
+
+# %%
+
+df = pd.concat([pd.DataFrame(y, columns=["Label"]), pd.DataFrame(X)], axis=1)
+
+df.loc[:,"Label"].value_counts()
+
+# %%
+
+df.groupby(by="Label").var()
+
+# %%
