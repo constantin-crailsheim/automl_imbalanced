d77cd59f7f9b37bea567d00b08daac7c13527cd2
Constantin von Crailsheim <constantin@vcrailsheim.de>
2023-03-09 19:06:13 +0100

    First attempts on AutoML model

--- ImbalancedAutoML.py
+++ ImbalancedAutoML.py
@@ -1,13 +1,49 @@
+from configuration import scoring
+
 from sklearn.base import ClassifierMixin
+
+from sklearn.model_selection import KFold, cross_val_score
+
+from sklearn import impute, pipeline, tree, ensemble
+from sklearn.metrics import balanced_accuracy_score
+
+from imblearn import under_sampling, over_sampling
 
 
 class ImbalancedAutoML(ClassifierMixin):
     def __init__(self) -> None:
         super().__init__()
 
-    def fit(self, X=None, y=None):
-        
-        raise (NotImplementedError)
+        self.hyperparameter = None
+
+        self.sampling_strategies = [
+            over_sampling.SMOTE(random_state=42),
+            under_sampling.TomekLinks(),
+            under_sampling.CondensedNearestNeighbour()]
+
+        self.model = pipeline.Pipeline(
+            steps=[
+                ("imputer", impute.SimpleImputer()),
+                ("estimator", ensemble.RandomForestClassifier()),
+            ]
+        )
+
+        self.cv = KFold(n_splits=2, shuffle=True, random_state=42)
+
+
+    def fit(self, X=None, y=None, verbose=False, path_results=None, time_limit=3600):
+
+        for sampling_strategy in self.sampling_strategies:
+            X, y = self.smote.fit_resample(X, y)
+            
+            # How to train model and pick correct model?
+
+            score = cross_val_score(self.model, X, y, scoring=scoring, cv=self.cv)
+
+            # self.model.fit(X, y)
+
+        return self
 
     def predict(self, X=None):
-        raise (NotImplementedError)
+
+        return self.model.predict(X)
--- experiments.py
+++ experiments.py
@@ -1,8 +1,13 @@
 # %%
 
-from data import Dataset
 import numpy as np
 import pandas as pd
+
+from data import Dataset
+from configuration import data_ids
+from sklearn import impute, pipeline, tree
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import balanced_accuracy_score
 
 # %%
 
@@ -11,14 +16,72 @@ data = Dataset.from_openml(976)
 X = data.features.to_numpy()
 y = data.labels.to_numpy()
 
-# %%
-
-df = pd.concat([pd.DataFrame(y, columns=["Label"]), pd.DataFrame(X)], axis=1)
-
-df.loc[:,"Label"].value_counts()
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 
 # %%
 
-df.groupby(by="Label").mean()
+from ImbalancedAutoML import ImbalancedAutoML
+
+automl = ImbalancedAutoML()
+
+score_vector = []
+
+for _ in range(1):
+    automl.fit(X_train, y_train)
+
+    y_eval = automl.predict(X_test)
+
+    score_vector.append(balanced_accuracy_score(y_test, y_eval))
+
+print(np.array(score_vector).mean())
+
+# %%
+
+for data_id in data_ids:
+    data = Dataset.from_openml(data_id)
+
+    X = data.features.to_numpy()
+    y = data.labels.to_numpy()
+
+    df = pd.concat([pd.DataFrame(y, columns=["Label"]), pd.DataFrame(X)], axis=1)
+
+    print(df.loc[:,"Label"].value_counts()/len(df))
+    
+
+# %%
+
+classtree = pipeline.Pipeline(
+    steps=[
+        ("imputer", impute.SimpleImputer()),
+        ("estimator", tree.DecisionTreeClassifier()),
+    ]
+)
+
+score_vector = []
+
+for _ in range(100):
+
+    classtree.fit(X_train, y_train)
+
+    y_eval = classtree.predict(X_test)
+
+    score_vector.append(balanced_accuracy_score(y_test, y_eval))
+
+print(np.array(score_vector).mean())
+
+
+# %%
+
+from imblearn import under_sampling, over_sampling
+
+sm = over_sampling.SMOTE(random_state=42)
+
+X_res, y_res = sm.fit_resample(X_train, y_train)
+
+classtree.fit(X_res, y_res)
+
+y_eval = classtree.predict(X_test)
+
+print(balanced_accuracy_score(y_test, y_eval))
 
 # %%
